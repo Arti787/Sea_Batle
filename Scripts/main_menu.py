278538c4a4bbf2f57b3ac9c374f37e282d84d0ce
GachiGriga <90316852+GachiGriga@users.noreply.github.com>
2022-02-04 22:53:07 +0300

    Тестовый код

--- Scripts/main_menu.py
+++ Scripts/main_menu.py
@@ -1,2 +1,40 @@
-from game_start import game_start
-    
+import pygame
+import sys
+# Переменные
+Width = 700
+Height = 800
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+
+
+class Button():
+
+    def __init__(self):
+        self.Nemo_Nom_Nom_Button = pygame.image.load("Images/Button.jpg")
+
+    def output(self, screen):
+        self.screen = screen
+
+
+
+if __name__ == '__main__':
+
+
+    pygame.init()
+    screen = pygame.display.set_mode((Width, Height))
+    pygame.display.set_caption("Летающие тарелки")
+
+    #СОЗДАЮ ЭКЗЕМПЛЯР КЛАССА
+    ButtonCL = Button()
+
+    while True:
+
+        ButtonCL.output(screen) #Вызов метода отрисовки кнопки
+
+
+        for event in pygame.event.get(): # Зкрытие программы при нажатии на крестик
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
